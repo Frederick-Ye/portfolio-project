fadda158d895353af744e40e4c98eb4553fb73d2
Frederick Ye <yenainglinn7@gmail.com>
2023-12-10 00:14:03 +0630

    fix linter error

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-const mobileMenu = document.querySelector(".mobile-menu");
-const openBtn = document.querySelector("#openMenuBtn");
-const closeBtn = document.querySelector("#closeMenuBtn");
-const portfolioBtn = document.getElementById("menu1");
-console.log(portfolioBtn)
-const aboutBtn = document.getElementById("menu2");
-const contactBtn = document.getElementById("menu3");
-
-openBtn.addEventListener("click", openFun)
-closeBtn.addEventListener("click", closeFun)
-portfolioBtn.addEventListener("click", closeFun)
-aboutBtn.addEventListener("click", closeFun)
-contactBtn.addEventListener("click", closeFun)
+const mobileMenu = document.querySelector('.mobile-menu');
+const openBtn = document.querySelector('#openMenuBtn');
+const closeBtn = document.querySelector('#closeMenuBtn');
+const portfolioBtn = document.getElementById('menu1');
+const aboutBtn = document.getElementById('menu2');
+const contactBtn = document.getElementById('menu3');
 
 function openFun(){
-  mobileMenu.classList.remove("hidden")
-  document.body.style.overflow = "hidden"
+  mobileMenu.classList.remove('hidden')
+  document.body.style.overflow = 'hidden'
 }
 
 function closeFun(){
-  mobileMenu.classList.add("hidden")
-  document.body.style.overflow = "auto"
+  mobileMenu.classList.add('hidden')
+  document.body.style.overflow = 'auto'
 }
+
+openBtn.addEventListener('click', openFun)
+closeBtn.addEventListener('click', closeFun)
+portfolioBtn.addEventListener('click', closeFun)
+aboutBtn.addEventListener('click', closeFun)
+contactBtn.addEventListener('click', closeFun)
+
