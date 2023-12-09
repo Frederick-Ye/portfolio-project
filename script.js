f64cdc44bd0b89a6a0cd95cb21b9d5fe02778c49
Frederick Ye <yenainglinn7@gmail.com>
2023-12-10 00:16:15 +0630

    fix linter error

--- script.js
+++ script.js
@@ -5,19 +5,18 @@ const portfolioBtn = document.getElementById('menu1');
 const aboutBtn = document.getElementById('menu2');
 const contactBtn = document.getElementById('menu3');
 
-function openFun(){
-  mobileMenu.classList.remove('hidden')
-  document.body.style.overflow = 'hidden'
+function openFun() {
+  mobileMenu.classList.remove('hidden');
+  document.body.style.overflow = 'hidden';
 }
 
-function closeFun(){
-  mobileMenu.classList.add('hidden')
-  document.body.style.overflow = 'auto'
+function closeFun() {
+  mobileMenu.classList.add('hidden');
+  document.body.style.overflow = 'auto';
 }
 
-openBtn.addEventListener('click', openFun)
-closeBtn.addEventListener('click', closeFun)
-portfolioBtn.addEventListener('click', closeFun)
-aboutBtn.addEventListener('click', closeFun)
-contactBtn.addEventListener('click', closeFun)
-
+openBtn.addEventListener('click', openFun);
+closeBtn.addEventListener('click', closeFun);
+portfolioBtn.addEventListener('click', closeFun);
+aboutBtn.addEventListener('click', closeFun);
+contactBtn.addEventListener('click', closeFun);
