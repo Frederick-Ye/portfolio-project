e331003d3724ec39f2acdf17f5a19ac8934f8d1f
Frederick Ye <yenainglinn7@gmail.com>
2023-12-10 21:00:08 +0630

    add popup windows

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ const closeBtn = document.querySelector('#closeMenuBtn');
 const portfolioBtn = document.getElementById('menu1');
 const aboutBtn = document.getElementById('menu2');
 const contactBtn = document.getElementById('menu3');
-
 function openFun() {
   mobileMenu.classList.remove('hidden');
   document.body.style.overflow = 'hidden';
@@ -41,3 +40,75 @@ window.addEventListener('scroll', () => {
     }
   });
 });
+
+// popup window 1
+const popUp1 = document.querySelector('.popupWindow1');
+const popupBtn1 = document.querySelector('#seeproject1');
+const closePopupBtn1 = document.querySelector('.a1');
+
+function openPopup1() {
+  popUp1.classList.remove('hidden');
+  document.body.style.overflow = 'hidden';
+}
+
+function closePopup1() {
+  popUp1.classList.add('hidden');
+  document.body.style.overflow = 'auto';
+}
+
+popupBtn1.addEventListener('click', openPopup1);
+closePopupBtn1.addEventListener('click', closePopup1);
+
+//popup window 2
+const popUp2 = document.querySelector('.popupWindow2');
+const popupBtn2 = document.querySelector('#seeproject2');
+const closePopupBtn2 = document.querySelector('.a2');
+
+function openPopup2() {
+  popUp2.classList.remove('hidden');
+  document.body.style.overflow = 'hidden';
+}
+
+function closePopup2() {
+  popUp2.classList.add('hidden');
+  document.body.style.overflow = 'auto';
+}
+
+popupBtn2.addEventListener('click', openPopup2);
+closePopupBtn2.addEventListener('click', closePopup2);
+
+//popup window 3
+const popUp3 = document.querySelector('.popupWindow3');
+const popupBtn3 = document.querySelector('#seeproject3');
+const closePopupBtn3 = document.querySelector('.a3');
+
+function openPopup3() {
+  popUp3.classList.remove('hidden');
+  document.body.style.overflow = 'hidden';
+}
+
+function closePopup3() {
+  popUp3.classList.add('hidden');
+  document.body.style.overflow = 'auto';
+}
+
+popupBtn3.addEventListener('click', openPopup3);
+closePopupBtn3.addEventListener('click', closePopup3);
+
+//popup window 4
+const popUp4 = document.querySelector('.popupWindow4');
+const popupBtn4 = document.querySelector('#seeproject4');
+const closePopupBtn4 = document.querySelector('.a4');
+
+function openPopup4() {
+  popUp4.classList.remove('hidden');
+  document.body.style.overflow = 'hidden';
+}
+
+function closePopup4() {
+  popUp4.classList.add('hidden');
+  document.body.style.overflow = 'auto';
+}
+
+popupBtn4.addEventListener('click', openPopup4);
+closePopupBtn4.addEventListener('click', closePopup4);
